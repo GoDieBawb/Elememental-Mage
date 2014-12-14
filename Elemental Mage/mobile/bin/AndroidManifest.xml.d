a9d37b67e6f283a3ec1c53f0dbe68f0d8a4ad406
Bawb <meancraft@hotmail.com>
2014-12-13 16:47:56 -0800

    Androiderized the Game
    Should work on Android
    Flipped all alpha maps on terrains
    Added Android Deployment
    Built first APK

---
+++ Elemental Mage/mobile/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Bawb\Documents\GitHub\Elememental-Mage\Elemental Mage\mobile\bin\AndroidManifest.xml : \
+C:\Users\Bawb\Documents\GitHub\Elememental-Mage\Elemental Mage\mobile\AndroidManifest.xml \
